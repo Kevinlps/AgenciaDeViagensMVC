5adbda82d4a278ed74f7c0b569eb1f82e17c19e9
Kevin Lopes Costa <88603868+Kevinlps@users.noreply.github.com>
2023-11-07 21:08:53 -0400

    Update ClienteController.java

--- AgenciaDeViagensMVC/src/main/java/principal/Controller/ClienteController.java
+++ AgenciaDeViagensMVC/src/main/java/principal/Controller/ClienteController.java
@@ -20,37 +20,37 @@ public class ClienteController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // Chame o método que lista todos os clientes (você deve implementar esse método)
+        
         ClienteDAO2 cdao = new ClienteDAO2();
         List<Cliente> clientes = cdao.listarClientes();
 
-        // Armazene a lista de clientes em um atributo do request para acessá-la na página JSP
+        
         req.setAttribute("clientes", clientes);
 
-        // Encaminhe a requisição para uma página JSP que exibirá a lista de clientes
-        RequestDispatcher dispatcher = req.getRequestDispatcher("listagemClientes.jsp"); // Substitua "listagemClientes.jsp" pelo nome da sua página JSP
+        
+        RequestDispatcher dispatcher = req.getRequestDispatcher("listagemClientes.jsp");
         dispatcher.forward(req, resp);
     }
 	
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // Recupere os parâmetros do formulário
-        String nome = req.getParameter("nome"); // Substitua "nome" pelo nome do campo no formulário
+       
+        String nome = req.getParameter("nome"); 
         String endereco = req.getParameter("endereco");
         String cpf = req.getParameter("cpf");
         String idadeStr = req.getParameter("idade");
-        String email = req.getParameter("email"); // Substitua "email" pelo nome do campo no formulário
+        String email = req.getParameter("email"); 
         
         int idade = Integer.parseInt(idadeStr);
-        // Crie um objeto Cliente com os parâmetros recebidos
-        Cliente cliente = new Cliente(nome,endereco, cpf, idade, email); // Supondo que você tenha uma classe Cliente com construtor correspondente
+        
+        Cliente cliente = new Cliente(nome,endereco, cpf, idade, email); 
 
-        // Chame o método que cria o cliente no banco de dados (você deve implementar esse método)
+        
         ClienteDAO2 cdao = new ClienteDAO2();
         cdao.criarCliente(cliente);
 
-        // Redirecione para uma página de sucesso ou faça o que for necessário
-        resp.sendRedirect("index.html"); // Substitua "sucesso.jsp" pela página que você deseja exibir após o cadastro
+        
+        resp.sendRedirect("index.html"); 
     }
     
     
